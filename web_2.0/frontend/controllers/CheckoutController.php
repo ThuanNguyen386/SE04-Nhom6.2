90d4d76321dd5864fd09e1fd0ae1c5a39417db89
hung <hungpy01199@example.com>
2020-12-15 01:04:42 +0700

    first commit

---
+++ web_2.0/frontend/controllers/CheckoutController.php
@@ -0,0 +1,41 @@
+<?php
+namespace frontend\controllers;
+use Yii;
+use yii\web\Controller;
+use frontend\components\Cart;
+use frontend\models\Checkout;
+use backend\models\OrderItem;
+
+class CheckoutController extends Controller {
+    function actionIndex() {
+        $model = new Checkout();
+        $cart = new Cart();
+        $cartstore = $cart->cartstore;
+        $cost = $cart->getCost;
+        if($model->load(Yii::$app->request->post())) {
+            $post = Yii::$app->request->post()['Checkout'];
+            foreach($cartstore as $item) :
+            {
+                $orderItem = new OrderItem;
+                $orderItem->buyer_name = $post['buyer_name'];
+                $orderItem->buyer_phone = $post['buyer_phone'];
+                $orderItem->buyer_address = $post['buyer_address'];
+                $orderItem->buy_date =  date('Y-m-d');
+                $orderItem->status = 1;
+                $orderItem->product_id = $item->id;
+                $orderItem->quantity = $item->qtt;
+                if($orderItem->save())
+                {
+                    echo("oke");
+                }
+            }
+            endforeach;
+        }   
+        return $this->render('index',[
+            'model' => $model,
+            'cartstore' => $cartstore,
+            'cost' => $cost,
+         ]);
+    }
+}
+?>
