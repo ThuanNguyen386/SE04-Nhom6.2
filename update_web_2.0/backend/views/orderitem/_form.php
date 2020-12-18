5ba63dfc846e1a9bfe765353972654714f00ab02
hung <hungpy01199@example.com>
2020-12-18 21:50:29 +0700

    update orderitem

---
+++ update_web_2.0/backend/views/orderitem/_form.php
@@ -0,0 +1,35 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model backend\models\Orderitem */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="orderitem-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'buyer_name')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'buyer_address')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'buyer_phone')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'product_id')->textInput() ?>
+
+    <?= $form->field($model, 'quantity')->textInput() ?>
+
+    <?= $form->field($model, 'buy_date')->textInput() ?>
+
+    <?= $form->field($model, 'status')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
---
+++ update_web_2.0/backend/views/orderitem/view.php
@@ -0,0 +1,43 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\DetailView;
+
+/* @var $this yii\web\View */
+/* @var $model backend\models\Orderitem */
+
+$this->title = $model->id;
+$this->params['breadcrumbs'][] = ['label' => 'Orderitems', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+\yii\web\YiiAsset::register($this);
+?>
+<div class="orderitem-view">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
+        <?= Html::a('Delete', ['delete', 'id' => $model->id], [
+            'class' => 'btn btn-danger',
+            'data' => [
+                'confirm' => 'Are you sure you want to delete this item?',
+                'method' => 'post',
+            ],
+        ]) ?>
+    </p>
+
+    <?= DetailView::widget([
+        'model' => $model,
+        'attributes' => [
+            'id',
+            'buyer_name',
+            'buyer_address',
+            'buyer_phone',
+            'product_id',
+            'quantity',
+            'buy_date',
+            'status',
+        ],
+    ]) ?>
+
+</div>
