be7a4d623b013f2cfaae20114223c16bc856cfce
DESKTOP-S09IBEJ\Dell <thuansahin@gmail.com>
2020-12-21 23:50:57 +0700

    merge

---
+++ backend/views/orderitem/index.php
@@ -0,0 +1,43 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel backend\models\search\OrderitemSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Orderitems';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="orderitem-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Create Orderitem', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'buyer_name',
+            'buyer_address',
+            'buyer_phone',
+            'product_id',
+            'quantity',
+            'buy_date',
+            'status',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+
+
+</div>
